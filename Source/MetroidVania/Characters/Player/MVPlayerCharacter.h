d4d6a9df077426ca3f9aca6119fd5808e3cf92b2
Michael Kenter <michael.kenter@gmail.com>
2022-05-15 19:33:08 -0700

    feat: initial commit

---
+++ Source/MetroidVania/Characters/Player/MVPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MetroidVania/Characters/MVCharacterBase.h"
+#include "MVPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METROIDVANIA_API AMVPlayerCharacter : public AMVCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MetroidVania/MetroidVaniaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetroidVaniaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METROIDVANIA_API AMetroidVaniaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
